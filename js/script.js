c7af0a7f53f4e7d0e0ee2742b0768f4f451214f2
sushil-rgb <sushil.bhandari002@gmail.com>
2023-05-19 02:55:15 +0545

    added effects

--- js/script.js
+++ js/script.js
@@ -4,8 +4,8 @@ Feel free to reach out using the contact details below. Cheers!`;
 const typingText = document.getElementById('typing-text');
 const socialIconsContainer = document.getElementById('social-icons-container');
 const icons = socialIconsContainer.getElementsByClassName('icon');
+const projectsSection = document.getElementById('projects');
 let charIndex = 0;
-
 
 function type() {
   typingText.innerHTML = text.slice(0, charIndex);
@@ -16,10 +16,11 @@ function type() {
     const nextChar = text.charAt(charIndex);
 
     // Adjust typing speed and delay for line breaks
-    const delay = (currentChar === '<' && nextChar === 'br' && text.charAt(charIndex + 2) === '>') ? 1000 : 50;
+    const delay = (currentChar === '<' && nextChar === 'b' && text.charAt(charIndex + 2) === 'r' && text.charAt(charIndex + 3) === '>') ? 1000 : 50;
 
     setTimeout(type, delay);
   } else {
+    fadeInProjectsSection();
     socialIconsContainer.style.opacity = '1';
     Array.from(icons).forEach((icon) => {
       icon.style.animation = 'fade-in 1s ease-in forwards';
@@ -27,8 +28,22 @@ function type() {
   }
 }
 
+function fadeInProjectsSection() {
+  projectsSection.style.opacity = '0';
+  projectsSection.style.display = 'block';
+
+  let opacity = 0;
+  const intervalId = setInterval(function() {
+    opacity += 0.1;
+    projectsSection.style.opacity = opacity.toString();
+
+    if (opacity >= 1) {
+      clearInterval(intervalId);
+    }
+  }, 100);
+}
+
 setTimeout(() => {
   typingText.style.opacity = '1'; // Show the text initially
   type();
 }, 1000); // Delay before starting the animation
-
