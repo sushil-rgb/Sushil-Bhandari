f58fdcab31bb225989c7c80a1fa91e88e78d69a1
sushil-rgb <sushil.bhandari002@gmail.com>
2023-05-18 15:41:11 +0545

    added effects

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
-const text = `👋 Hey there! I'm Sushil, your go-to guy for web scraping and automation. If you've got a project in mind, let's connect and discuss your requirements. Feel free to reach out using the contact details below. Cheers!`;
+const text = `👋 👋 Hey there! I'm Sushil, your go-to guy for web scraping and automation. If you've got a project in mind, let's connect and discuss your requirements. 
+Feel free to reach out using the contact details below. Cheers!`;
+
 const typingText = document.getElementById('typing-text');
+const socialIconsContainer = document.getElementById('social-icons-container');
+const icons = socialIconsContainer.getElementsByClassName('icon');
 let charIndex = 0;
 
 
@@ -15,9 +19,13 @@ function type() {
     const delay = (currentChar === '<' && nextChar === 'br' && text.charAt(charIndex + 2) === '>') ? 1000 : 50;
 
     setTimeout(type, delay);
+  } else {
+    socialIconsContainer.style.opacity = '1';
+    Array.from(icons).forEach((icon) => {
+      icon.style.animation = 'fade-in 1s ease-in forwards';
+    });
   }
 }
-
 
 setTimeout(() => {
   typingText.style.opacity = '1'; // Show the text initially
